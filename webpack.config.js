de46484b295fa75721721a4950e80240c5dfce9a
travis <travis.x.wong@gmail.com>
2020-09-07 09:40:29 +0800

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path');
 
 module.exports = {
@@ -6,5 +7,20 @@ module.exports = {
   output: {
     // path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      // Load a custom template (lodash by default)
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   }
 };
